3be36085af9deb1f31e43dc31bbb7be0ba676b73
Maik Hentsche <maik.hentsche@amd.com>
2012-07-05 11:38:53 +0200

    [FIX] new color generation

--- simple.pl
+++ simple.pl
@@ -4,46 +4,53 @@ use common::sense;
 
 use SDLx::App;
 use constant { 
-        ZYKLEN           => 8000,
-        DIVERGENZGRENZE  => 1.e23,
-        KONVERGENZGRENZE => 1.e-23,
+        ZYKLEN           => 1200,
 };
 
 
-sub farbe
+sub color
 {
         my ($x, $y) = @_;
-        my ($a, $b);
-        my ($qua, $qub);
-        my $qu;
-        my $zaehler;
+        my ($real, $imag, $c);
+        my $counter;
 
-        $a = $x; $b = $y;
-        $zaehler = 0;
-        $qua = $a * $a; $qub = $b * $b;
-        do {
-                $b = 2 * $a * $b - $y;
-                $a = $qua - $qub - $x;
-                $qu = ($qua = $a * $a) + ($qub = $b * $b);
-        } while ($qu < DIVERGENZGRENZE  and
-                 $qu > KONVERGENZGRENZE and
-                 $zaehler++ < ZYKLEN);
-        return($zaehler & 0xff);
+        $real = $x; $imag = $y;
+        $counter = 0;
+        my $counter;
+        for (1..ZYKLEN) {
+                $counter=$_;
+                $imag = 2 * $real * $imag - $y;
+                $real = $real**2 - $imag**2 - $x;
+                $c = sqrt($real**2 + $imag**2);
+                last if $c > 2;
+        }
+        if ($counter >= ZYKLEN) {
+                # I want the Mandelbrot set to be colored black
+                return 0;
+        } else {
+                # Idea behind this: Since we have 32bit color depth, we
+                # have 2**32 color values. We devide the whole color set
+                # into as many subsets as $counter can have values. Now
+                # $counter tells us which subset to take and from this
+                # subset we always use the middle element.
+                my $size_part    = 2**32 / ZYKLEN;
+                my $choosen_part = ($counter * $size_part + ($counter - 1) * $size_part) / 2;
+                return int $choosen_part;
+        }
 }
 
-my $app = SDLx::App->new( h => 80,
-                          w => 120,
+my $app = SDLx::App->new( h => 4*100,
+                          w => 6*100,
                         );
 
 for (my $x=0; $x<=$app->width; $x++) {
-        my $real = -2.0+$x*(4.0/$app->width);
+        my $real = -2.0+$x*(3.0/$app->width);
         for (my $y=0; $y<=$app->height; $y++ ) {
                 my $imag = -1.0+$y*(2.0/$app->height);
-                my $farbe = farbe($real, $imag);
-                $app->[$x][$y] = [ $farbe, $farbe, $farbe, 255];
+                my $color = color($real, $imag);
+                $app->[$x][$y] = $color;
         }
-        
+        $app->update;
 }
-$app->update;
-
+say "done";
 my $line = <>;
